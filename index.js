5d0acadd3d806f294f95f3d41fc53992baad63ed
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2023-11-04 00:40:20 -0500

    feat: :construction: building aleatory emails

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { app } from "./app.js"
+import { AleatoryEmailComponent } from "./components/aleatory-email-component.js";
 
 addEventListener("DOMContentLoaded", async (e) => {
     await app();
